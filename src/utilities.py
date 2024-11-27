f8958332826c2cf1605a60a42bf08b915d9e2e0b
Daniel Richards <danrichards678@gmail.com>
2024-11-27 11:32:28 +0000

    Added writevtk function

--- src/utilities.py
+++ src/utilities.py
@@ -1,5 +1,6 @@
 import numpy as np
-from dolfinx import fem
+from dolfinx import fem, io
+from mpi4py import MPI
 
 
 def move_mesh(msh,uh,k):
@@ -14,3 +15,34 @@ def mesh_sizes(mesh):
     h = mesh.h(tdim,np.arange(num_cells))
     return h
 
+
+def write_vtk(filename,msh,functions,names,t=0.0):
+
+    for idx,f in enumerate(functions):
+        # check if has function space
+        if hasattr(f,"ufl_function_space"):
+            if f.ufl_element().degree == 1:
+                functions[idx].name = names[idx]
+            else:
+                # Interpolate onto order 1
+                Q = fem.functionspace(msh, ("Lagrange", 1, f.ufl_shape))
+                temp = fem.Function(Q)
+                temp.interpolate(fem.Expression(f,Q.element.interpolation_points()))
+                temp.name = names[idx]
+                functions[idx] = temp
+
+        else:
+            Q = fem.functionspace(msh, ("Lagrange", 1, f.ufl_shape))
+            temp = fem.Function(Q)
+            temp.interpolate(fem.Expression(f,Q.element.interpolation_points()))
+            temp.name = names[idx]
+            functions[idx] = temp
+
+
+
+
+
+
+    with io.VTKFile(MPI.COMM_WORLD, filename, "w") as file:
+        file.write_mesh(msh)
+        file.write_function(functions,t)
