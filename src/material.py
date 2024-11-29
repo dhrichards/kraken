0531b72a4e24a34ca3c198fece581afb07e679e3
Daniel Richards <danrichards678@gmail.com>
2024-11-29 09:44:42 +0000

    added option to remove uc

--- src/material.py
+++ src/material.py
@@ -4,6 +4,7 @@ secperyr = 60*60*24*365.25
 
 class MaterialProperties:
     def __init__(self, ρi=900, ρw=1000, g=9.81, E=9.33e9, ν=0.325, A=1.2e-25, n=3.0, Gc=1.0, ψcrit=1.0, l=5e-3, uc=1e-2, L=100.0, τ=secperyr):
+
         # Default material properties
         self.ρi = ρi # Density of ice
         self.ρw = ρw # Density of water
@@ -24,6 +25,9 @@ class MaterialProperties:
 
         self.Hc = self.μ*(uc/L)**2
         self.ψcritstar = self.ψcrit / self.Hc
+        self.pc = self.μ * uc / L
+        self.pwc = self.ρw * self.g * L
+
         
 
 
@@ -78,6 +82,72 @@ class MaterialProperties:
         """Set the regularisation length from the mesh."""
         h = mesh_sizes(msh)
         self.l = 2*h.max()
+
+
+class Material_no_uc:
+    def __init__(self, ρi=900, ρw=1000, g=9.81, E=9.33e9, ν=0.325, A=1.2e-25, n=3.0, Gc=1.0, ψcrit=1.0):
+
+        # Default material properties
+        self.ρi = ρi # Density of ice
+        self.ρw = ρw # Density of water
+        self.g = g # Gravitational acceleration
+        self.E = E # Young's modulus
+        self.ν = ν # Poisson's ratio
+        self.A = A # Flow law parameter
+        self.n = n # Flow law exponent
+        self.Gc = Gc # Fracture toughness
+        self.ψcrit = ψcrit 
+
+        # Choose L, τ such that C1, C2, C3*l = 1 by default
+        self.L = self.μ/(self.ρw*self.g)
+        self.τ = (self.A**(1/self.n) * self.μ)**-self.n
+        self.l = 1/self.C3
+
+        self.Hc = self.μ**2
+        self.ψcritstar = self.ψcrit / self.Hc
+        self.pc = self.μ 
+        self.pwc = self.ρw * self.g * self.L
+        self.ρratio = self.ρi/self.ρw
+        
+
+
+    @property
+    def λ(self):
+        """Calculate the first Lamé parameter."""
+        return self.ν * self.E / ((1 + self.ν) * (1 - 2 * self.ν))
+
+    @property
+    def μ(self):
+        """Calculate the shear modulus (second Lamé parameter)."""
+        return self.E / (2 * (1 + self.ν))
+    
+    @property
+    def C1(self):
+        """Non dimensional constant describing ratio between
+        external and elastic stresses."""
+        return self.L * self.ρw * self.g / (self.μ)
+    
+    @property
+    def C2(self):
+        """Non dimensional constant describing ratio between
+        elastic and viscousc stresses."""
+        return (self.A*self.τ)**(1/self.n) * self.μ 
+    
+    @property
+    def C3(self):
+        """Non dimensional constant describing ratio between
+        elastic and fracture stresses."""
+        return self.μ * self.L / self.Gc 
+    
+
+
+    def set_l_from_mesh(self,msh):
+        """Set the regularisation length from the mesh."""
+        h = mesh_sizes(msh)
+        self.l = 2*h.max()
+
+
+
     
 
 
