5faeedbd0d9c5789c9b43d693d5f0eeb1d995684
Daniel Richards <dancha@bsl-ws-s10.nerc-bas.ac.uk>
2025-06-25 10:22:20 +0100

    updated hpc script

--- scripts/hpcscript.sh
+++ scripts/hpcscript.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --output=/users/dancha/jobs/myfirstjob.%j.%N.out
-#SBATCH --error=/users/dancha/jobs/myfirstjob.%j.%N.err
+#SBATCH --output=/users/dancha/jobs/hires.%j.%N.out
+#SBATCH --error=/users/dancha/jobs/hires.%j.%N.err
 #SBATCH --job-name=icebergtest
-#SBATCH --ntasks=2
+#SBATCH --ntasks=8
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=128gb
+#SBATCH --mem=256gb
 #SBATCH --nodes=1
-#SBATCH --time=00:15:00
+#SBATCH --time=00:30:00
 #SBATCH --partition=rocky
 #SBATCH --account=rocky
 #SBATCH --mail-type=begin,end,fail,requeue
@@ -17,10 +17,10 @@
 
 . /users/dancha/spack/share/spack/setup-env.sh
 spack env activate fenicsx
-cd /users/dancha/kraken3/scripts
+cd /users/dancha/kraken3
 
 
 mpirun --version
 
 
-srun -n 2 python3 iceberg_hpc.py
+mpirun -n $SLURM_NTASKS python3 scripts/iceberg.py
