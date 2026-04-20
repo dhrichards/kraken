d1019868ffff85d6257e7f00105b50edeb65c090
Daniel Richards <danrichards678@gmail.com>
2026-04-20 14:13:08 +0100

    small changes

--- scripts/iceberg.py
+++ scripts/iceberg.py
@@ -257,7 +257,7 @@ if MPI.COMM_WORLD.rank == 0:
 
 model.damage_on = False
 if args.type == "relaxation":
-    i_start = 10
+    i_start = 30
     model.params.dt.value = 25*24*60*60
 else:
     i_start = 1
